09fcf21a7d915941aca4ec9cefbfd84c4945ab09
fmstacco <fmstacco@gmail.com>
2022-06-22 09:31:20 +0000

    Add questions for the quiz on the questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,65 @@
+const questions = [
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, alias!",
+    "answer1": "1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer1Total": "1",
+    "answer2": "2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer2Total": "2",
+    "answer3": "3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ullam esse, iste porro quae perspiciatis at error ipsa ea eos?",
+    "answer3Total": "3"
+  },
+]
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//Set of questions and answers 
+//Give each answer an identifier 
+//Each identifier will increment through each question
+//St the end the identifier with the highest number is the winner 
+//Display the answer and rest the quiz
+
+//pass results frm previous question to the next page usig localcache
+
+//Randomise the background of the quiz for each questiion
+
+//Possible Results 
+// 15 -21- You Need Help
+// 10 - 15 - Good Soul
+// 5- 10 - Meh 
+// 5 - Are You Even Real
+
