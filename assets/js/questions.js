0e8fa4286e9fb21e69be222a906a82adf6d29693
fmstacco <fmstacco@gmail.com>
2022-07-02 21:28:31 +0000

    Add data questions to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,28 +1,28 @@
 const questions = [
   {
-  	question:'Which month comes right before june ?',
-  	options:['may','september','july','august'],
-  	answer:0
-  },
-  {
-  	question:'What color is a banana?',
-  	options:['red','yellow','white','blue'],
-  	answer:1
-  },
-  {
-  	question:'3 + 4 = 7 ?',
-  	options:['true','false'],
-  	answer:0
-  },
-  {
-  	question:'What time of day do we have breakfast?',
-  	options:['In the afternoon','In the evening','In the morning'],
-  	answer:2
-  },
-  {
-  	question:'What is 22 + 6?',
-  	options:['99','56','16','28'],
+  	question:'Where does your company keep analytical data?',
+  	options:['We mainly keep analytical data in spreadsheets and on local PCs.','We store analytical data in siloed databases across our organization.','Our analytical data is stored in data lakes, and smart IoT devices even collect, process, store and communicate data.','We store analytical data in a central data warehouse with an extract, transform and load process (ETL).'],
   	answer:3
+  },
+  {
+  	question:'How does your company manage data??',
+  	options:['The right data management is ensured through data governance over the entire life-cycle of the data, speeding up data-driven innovation instead of hindering it.','We collect data on a small scale, and we have no governance in place.','We have basic governance to help ensure data quality and security.','We collect data and have in place ad hoc governance.'],
+  	answer:0
+  },
+  {
+  	question:'Does your company have any GPDP control?',
+  	options:['No','Yes, with IT department', 'Yes, implemented', 'Yes, implemented with Governance Department' ],
+  	answer:0
+  },
+  {
+  	question:'Does your company use strategic planning tools or methodologies?',
+  	options:['Yes (eg Crisp-DM, SaaS, Agile, etc.)','Yes, Excel','No','Yes, on premise/cloud system'],
+  	answer:0
+  },
+  {
+  	question:'What departaments of the company are data-driven?',
+  	options:['All departments of the company.','Management','Finance','Commercial', 'Marketing', 'Human Resources', 'Operations'],
+  	answer:0
   },
   {
     question:'How many squares are there in the following figure?',
